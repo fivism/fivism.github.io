3e25a819a11fde4ddc88fad28d7c96b36c0fddd7
fivism <michael.fivis@gmail.com>
2018-09-23 13:59:20 +0200

    Tag

--- spec/jekyll-last-modified-at/executor_spec.rb
+++
@@ -1,7 +0,0 @@
-require 'spec_helper'
-
-describe(Jekyll::LastModifiedAt::Executor) do
-  it "gets and strips the output" do
-    expect(described_class.sh('echo', 'ohai')).to eql('ohai')
-  end
-end
